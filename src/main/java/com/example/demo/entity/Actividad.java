27d2b23614ce86e1439916ccabec1e4e47c0ea37
Ali Lopez <a20167442@pucp.edu.pe>
2021-04-19 23:05:34 -0500

    -/-

--- src/main/java/com/example/demo/entity/Actividad.java
+++ src/main/java/com/example/demo/entity/Actividad.java
@@ -1,4 +1,15 @@
 package com.example.demo.entity;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Actividad {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idproyecto;
+    private String nombreproyecto;
+    @Column(nullable = false)
+    private String usuario_owner;
 }
