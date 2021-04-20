7d46f697e12bf431fcc6279335da58dbf88fd1b4
Jose Garcia - Acer <garcia.josea@pucp.edu.pe>
2021-04-20 00:20:40 -0500

    avance José usuario

--- src/main/java/com/example/demo/controller/UsuarioController.java
+++ src/main/java/com/example/demo/controller/UsuarioController.java
@@ -1,43 +1,49 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.Usuario;
+import com.example.demo.repository.AreaRepository;
 import com.example.demo.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Optional;
 
 @Controller
+@RequestMapping("/usuario")
 public class UsuarioController {
+
     @Autowired
     UsuarioRepository usuarioRepository;
 
+    @Autowired
+    AreaRepository areaRepository;
 
-
-    @GetMapping("/usuario/listar")
+    @GetMapping("/listar")
     public String usuarioLista(Model model){
 
         model.addAttribute("usuarioList", usuarioRepository.findAll());
         return "usuario/list";
     }
-    @GetMapping("/usuario/new")
-    public  String usuarioNew(){
+    @GetMapping("/new")
+    public  String usuarioNew(Model model){
+        model.addAttribute("listaAreas", areaRepository.findAll());
         return "usuario/newForm";
     }
 
 
-    @PostMapping("/usuario/save")
+    @PostMapping("/guardar")
     public  String usuarioSave (Usuario usuario, RedirectAttributes attr){
         usuarioRepository.save(usuario);
         attr.addFlashAttribute("msg", "Usuario creado exitosamente");
-        return "redirect:/usuario";
+        return "redirect:/usuario/listar";
     }
-    @GetMapping("/usuario/edit")
+    @GetMapping("/edit")
     public  String usuarioEdit(@RequestParam("correo") String correo, Model model){
         Optional<Usuario> usuarioOptional = usuarioRepository.findById(correo);
         if(usuarioOptional.isPresent()){
@@ -49,7 +55,7 @@ public class UsuarioController {
         }
     }
 
-    @GetMapping ("/shipper/delete")
+    @GetMapping ("/delete")
     public  String deleteUsuario(@RequestParam("correo") String correo){
         Optional<Usuario> usuarioOptional =usuarioRepository.findById(correo);
         if(usuarioOptional.isPresent()){
